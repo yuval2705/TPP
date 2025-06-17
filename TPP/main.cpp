edc7af931d109ae2a2e9b3c141037d735e29c1f3
yuval rozen <yuvalrozen27@gmail.com>
2025-06-17 15:10:47 +0300

    finish implementing the printing mec

--- TPP/main.cpp
+++ TPP/main.cpp
@@ -8,38 +8,47 @@ enum ReturnCodes
 
 const int TABLE_WIDTH = 11;
 const int TABLE_HEIGHT = 11;
-
+const int CELL_WIDTH = 7;
 
 /*
 * Prints a single row of the multipication table.
 * 
 * @param row_value [IN]: The current row value.
 * @param width [IN]: The width of the row to print.
+* @param cell_width [IN]: The width of each cell.
 */
-void printTableRow(int row_value, int width) {
+void printTableRow(int row_value, int width, int cell_width) {
     std::string row = "";
-    for (int i = 0; i < width; i++) {
-        row += "  ";
+    for (int i = 0; i <= width; i++) {
+        std::string curr_cell = "";
+
         if (row_value == 0 && i == 0) {
-            row += " ";
+            curr_cell += " ";
         }
         else if (row_value == 0)
         {
-            row += std::to_string(i);
+            curr_cell += std::to_string(i);
         }
         else if (i == 0)
         {
-            row += std::to_string(row_value);
+            curr_cell += std::to_string(row_value);
         }
         else
         {
-            row += std::to_string(i * row_value);
+            curr_cell += std::to_string(i * row_value);
         }
-        row += "  |";
+
+        for (int i = curr_cell.length(); i < CELL_WIDTH; i++)
+        {
+            curr_cell += " ";
+        }
+        
+        row += curr_cell;
+        row += "|";
     }
 
     std::string divider = "";
-    for (int i = 0; i < row.length(); i++)
+    for (int i = 0; i <= row.length(); i++)
     {
         divider += "-";
     }
@@ -52,16 +61,16 @@ void printTableRow(int row_value, int width) {
  * 
  * @param width [IN]: The width of the table to print.
  * @param heigth [IN]: The height of the table to print.
+ * @param cell_width [IN]: The width of each cell.
  */
-void printMultipicationTable(int width, int height)
+void printMultipicationTable(int width, int height, int cell_width)
 {
     for (int i = 0; i <= height; i++) {
-        printTableRow(i, width);
+        printTableRow(i, width, cell_width);
     }
 }
 
-
 void main()
 {
-    printMultipicationTable(TABLE_WIDTH, TABLE_HEIGHT);
+    printMultipicationTable(TABLE_WIDTH, TABLE_HEIGHT, CELL_WIDTH);
 }
