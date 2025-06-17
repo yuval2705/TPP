4bef72234ae2a0bdb21c5598633434430dd6fe68
yuval rozen <yuvalrozen27@gmail.com>
2025-06-17 11:56:22 +0300

    Add the declartion of the print func

---
+++ TPP/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+enum ReturnCodes {
+	SUCCESS = 0
+};
+
+/*
+* Prints the multipication table in the given dims.
+*/
+
+void printMultipicationTable(int length, int height) {
+}
