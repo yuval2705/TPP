1c509e8feac00adf333b91c420fe64172d1407ce
yuval rozen <yuvalrozen27@gmail.com>
2025-06-17 14:36:18 +0300

    continue of the implemention

--- TPP/main.cpp
+++ TPP/main.cpp
@@ -6,35 +6,62 @@ enum ReturnCodes
     SUCCESS = 0
 };
 
-
 const int TABLE_WIDTH = 11;
 const int TABLE_HEIGHT = 11;
 
 
-void printTableRow(int width) {
+/*
+* Prints a single row of the multipication table.
+* 
+* @param row_value [IN]: The current row value.
+* @param width [IN]: The width of the row to print.
+*/
+void printTableRow(int row_value, int width) {
     std::string row = "";
-    for (int i = 1; i < width; i++) {
+    for (int i = 0; i < width; i++) {
         row += "  ";
-        row += i;
+        if (row_value == 0 && i == 0) {
+            row += " ";
+        }
+        else if (row_value == 0)
+        {
+            row += std::to_string(i);
+        }
+        else if (i == 0)
+        {
+            row += std::to_string(row_value);
+        }
+        else
+        {
+            row += std::to_string(i * row_value);
+        }
         row += "  |";
     }
 
     std::string divider = "";
     for (int i = 0; i < row.length(); i++)
     {
-        divider = "-";
+        divider += "-";
     }
-
+    std::cout << row << std::endl;
+    std::cout << divider << std::endl;
 }
 
 /*
  * Prints the multipication table in the given dims.
+ * 
+ * @param width [IN]: The width of the table to print.
+ * @param heigth [IN]: The height of the table to print.
  */
 void printMultipicationTable(int width, int height)
 {
-    std::cout << "|    |";
-    printTableRow(width);
-    for (int i = 1; i <= height; i++) {
-
+    for (int i = 0; i <= height; i++) {
+        printTableRow(i, width);
     }
+}
+
+
+void main()
+{
+    printMultipicationTable(TABLE_WIDTH, TABLE_HEIGHT);
 }
