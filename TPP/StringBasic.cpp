df8c0b512ea1264d0a3ff382febf248e25abbfd2
yuval rozen <yuvalrozen27@gmail.com>
2025-06-18 18:33:05 +0300

    add more functionality to the StringBasic class.

--- TPP/StringBasic.cpp
+++ TPP/StringBasic.cpp
@@ -1,6 +1,7 @@
 #include <stdlib.h>
-#include "StringBasic.h"
 #include <string>
+#include <exception>
+#include "StringBasic.h"
 
 StringBasic::StringBasic(char* str)
 {
@@ -25,7 +26,7 @@ StringBasic::StringBasic(char chr) : StringBasic("-")
     this->m_str[0] = chr;
 }
 
-StringBasic::StringBasic(StringBasic& str) : StringBasic(str.getStr())
+StringBasic::StringBasic(const StringBasic& str) : StringBasic(str.getStr())
 {
     // This is empty on purpose! We want the same things to happen for each type.
 }
@@ -43,4 +44,58 @@ char* StringBasic::getStr() const
 StringBasic::~StringBasic()
 {
     delete[] this->m_str;
+}
+
+bool StringBasic::isEmpty() const
+{
+    return (this->m_length == 0) || (this->m_str == nullptr);
+}
+
+char& StringBasic::operator[](std::size_t i)
+{
+    if (i < 0 || i >= this->m_length)
+    {
+        throw std::runtime_error("Index is out of bounds!");
+    }
+
+    return this->m_str[i];
+}
+
+int StringBasic::compare(char* str) const
+{
+    return std::strcmp(this->m_str, str);
+}
+
+int StringBasic::compare(const StringBasic& str) const
+{
+    return this->compare(str.getStr());
+}
+
+int StringBasic::compare(const char str[]) const
+{
+    return this->compare((char*)str);
+}
+
+void StringBasic::append(const char* str)
+{
+    unsigned appended_str_length = (unsigned)std::strlen(str);
+
+    char* new_m_str = new char[this->m_length + appended_str_length + 1]{0};
+    std::memcpy(new_m_str, this->m_str, this->m_length);
+    std::memcpy(new_m_str + this->m_length, str, appended_str_length);
+    delete[] this->m_str;
+    this->m_str = new_m_str;
+    this->m_length = appended_str_length + this->m_length;
+}
+
+void StringBasic::append(const StringBasic& str)
+{
+    this->append(str.getStr());
+}
+
+unsigned StringBasic::replace(char oldC, char newC)
+{
+    char* swappingPointer = std::strchr(this->m_str, oldC);
+    *swappingPointer = oldC;
+    return (unsigned) (swappingPointer - this->m_str);
 }
--- TPP/main.cpp
+++ TPP/main.cpp
@@ -19,4 +19,17 @@ void main()
     StringBasic e = a;
     std::cout << e.getLength() << std::endl;
     std::cout << e.getStr() << std::endl;
+
+
+    std::cout << a.compare(e) << std::endl;
+    std::cout << a.compare("hello, world") << std::endl;
+    std::cout << a.compare("hello, ") << std::endl;
+
+    std::cout << a.isEmpty() << std::endl;
+    std::cout << b.isEmpty() << std::endl;
+
+    a.append("i am appended");
+    std::cout << a.getStr() << std::endl;
+
+    std::cout << a[7] << std::endl;
 }
